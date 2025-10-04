13f4b0ad77778278d1a1e4fc8a1adc194cc189dd
Suhail <suhailjho139@gmail.com>
2025-10-04 16:39:22 +0530

    Update NextAuth Google signIn logic

--- src/app/admin/collections/Your-collections/page.js
+++ src/app/admin/collections/Your-collections/page.js
@@ -1,5 +1,5 @@
 "use client";
-
+export const dynamic = "force-dynamic";
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { useRouter, useSearchParams } from "next/navigation";
